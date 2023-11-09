e0bd4a45593eb18380de44d850375c0a262247c5
muhammeddemrc <mdemirci.ali@gmail.com>
2023-11-09 14:23:28 +0300

    main

---
+++ loveProject.js
@@ -0,0 +1,14 @@
+var you = prompt("What is your name ?");
+
+var yourlove = prompt("What is your love name ?");
+
+var num = Math.random();
+
+num = num*100 + 1;
+
+num = Math.floor(num);
+    
+
+
+
+console.log(you + " ve "+ yourlove + " aşk oranınınız = " + num);
